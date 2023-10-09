908c22e4f88d96314af2af0079a2cd552813e639
amiqiu <amiqiu@foxmail.com>
2023-10-09 17:41:18 +0800

    第一次上传C语言文件

---
+++ C_231009/test.c
@@ -0,0 +1,92 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//	printf("hehe");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("hehe");
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	printf("hello world\n");//输出
+//	printf("你好，C语言\n");//输出
+//
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	//占位符
+//	printf("%d\n", 100);//%d - 整型
+//	printf("%c\n", 'q');//%c  - 字符
+//	printf("%f\n", 3.14f);//%f  - 浮点数（小数）
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("%c\n", 'A');
+//	printf("%c\n", 65);
+//
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	//32~127
+//	int i = 0;
+//	//产生32~127的数值
+//	for (i=32; i<=127; i++)
+//	{
+//		printf("%c ", i);//将i中的数值当做ASCII码值，直接打印对应的字符
+//	}
+//
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	//printf("hehe\n");
+//	//printf("haha\n");
+//	//printf("%s\n", "hello");//%s - 字符串
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	//char arr1[] = "abc";
+//	//char arr2[] = {'a', 'b', 'c'};//字符串中有一个结束标志叫：\0
+//
+//	//printf("%s\n", arr1);
+//	//printf("%s\n", arr2);
+//	printf("abc\0def");
+//
+//	return 0;
+//}
+
+
+int main()
+{
+	printf("abc\ndef");
+
+	return 0;
+}
